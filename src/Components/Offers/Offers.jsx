53cfd0f57b0aec30d7b5066264c56d22e6d99c3e
Ahmad Bahr <ahmad.bahr@mubs.edu.lb>
2023-12-22 11:00:45 +0200

    Updates

--- src/Components/Offers/Offers.jsx
+++ src/Components/Offers/Offers.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import './Offers.css'
 import exclucive_image from '../Assets/exclusive_image.png'
@@ -18,27 +17,4 @@ const Offers = () => {
     </div>
   )
 }
-
-=======
-import React from 'react'
-import './Offers.css'
-import exclusive_image from '../Assets/exclusive_image.png'
-
-const Offers = () => {
-  return (
-    <div className='offers'>
-        <div className="offers-left">
-                <h1>Exclusive</h1>
-                <h1>Offer For You</h1>
-                <p>ONLY ON BEST SELLERS PRODUCT</p>
-                <button>Check Now</button>
-        </div>
-        <div className="offers-right">
-            <img src={exclusive_image} alt=""/>
-        </div>
-        </div>
-  )
-}
-
->>>>>>> 78f4f7c3525f0fbde691745bd9743d511f89ece8
 export default Offers
