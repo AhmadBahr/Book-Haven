c9aba083017fdfa75d269dec1fc2b165a65e3df9
Ahmad Bahr <ahmad.bahr@mubs.edu.lb>
2023-12-23 23:24:08 +0200

    Login/Signup is done

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
+import '../Pages/CSS/LoginSignup.css'
 
 const LoginSignup = () => {
   return (
     <div className='loginsignup'>
       <div className="loginsignup-container">
           <h1>Signup</h1>
-          <div className="loginsignup-fileds">
-            
+          <div className="loginsignup-fields">
+            <input type="text" placeholder='Your Name'/>
+            <input type="email" placeholder='Email Address'/>
+            <input type="password" placeholder='Password'/>
           </div>
-      </div>
-    </div>
+          <button>Register</button>
+          <p className="loginsignup-login">Already have an account? <span>Login here</span></p>
+              <div className="loginsignup-agree">
+                <input type="checkbox" name='' id=''/>
+                <p>By continuing, i agrree to the terms of use & privacy policy</p>
+              </div>
+            </div>
+          </div>
   )
 }
 
