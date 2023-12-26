3a73a705afe6abb898c6a6f9a3c6cf0868769e8e
Ahmad Bahr <ahmad.bahr@mubs.edu.lb>
2023-12-27 00:04:55 +0200

    Project Overhauled

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,35 +1,58 @@
 import React from 'react';
-import './Footer.css'
+import './Footer.css';
 import footer_logo from '../Assets/logo_big.png';
-import instagram_icon from '../Assets/instagram_icon.png';
-import pintester_icon from '../Assets/pintester_icon.png';
-import whatsapp_icon from '../Assets/whatsapp_icon.png';
+
 
 export const Footer = () => {
   return (
     <div className='footer'>
+      <br/><br/>
       <div className="footer-logo">
         <img src={footer_logo} alt="" />
-        <p>SHOPPER</p>
       </div>
-      <ul className="footer-links">
-        <li>Company</li>
-        <li>Products</li>
-        <li>Offices</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
-      <div className="footer-social-icon">
-        <div className="footer-icons-container">
-          <img src={instagram_icon} alt="" />
+      <div className="footer-links">
+        <div className="customer-section">
+          <p>CUSTOMER</p>
+          <ul className="sub-links">
+            <li>Log In</li>
+            <li>My Account</li>
+            <li>Terms and Conditions</li>
+          </ul>
         </div>
-        <div className="footer-icons-container">
-          <img src={pintester_icon} alt="" />
+        <div className="quick-links">
+          <p>QUICK LINKS</p>
+          <ul className="sub-links">
+            <li>Explore Books</li>
+            <li>About Asala</li>
+            <li>View Catalog</li>
+          </ul>
         </div>
-        <div className="footer-icons-container">
-          <img src={whatsapp_icon} alt="" />
+        <p>SOCIAL MEDIA</p>
+        <div className="social-media-icons">
+          <div className="footer-icons-container">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3-PwlexbeDW56TRqSrv86oQ7GGzrAUYJCNr3ihi0cK1H6gkoorAHNAnr67rLlddJc5VI&usqp=CAU" alt="Facebook" />
+          </div>
+          <div className="footer-icons-container">
+            <img src="https://static-00.iconduck.com/assets.00/instagram-rounded-filled-icon-2047x2048-sc0zywj5.png" alt="Instagram" />
+          </div>
+          <div className="footer-icons-container">
+            <img src="https://www.svgrepo.com/show/332182/twitter-circle.svg" alt="Twitter" />
+          </div>
+          <div className="footer-icons-container">
+            <img src="https://cdn-icons-png.flaticon.com/512/80/80648.png" alt="Pinterest" />
+          </div>
+        </div>
+        <div className="quick-links">
+          <p>Call US</p>
+          <ul className="sub-links">
+            <li>Beirut:</li>
+            <p>+9611 83 3217</p>
+            <li>Istanbul:</li>
+            <p>+931 1 83 32 17</p>
+          </ul>
         </div>
       </div>
+      <hr/>
       <div className="footer-copyright">
         <hr />
         <p>Copyright @ 2023 - All Rights Reserved</p>
--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -6,20 +6,75 @@ const LoginSignup = () => {
   return (
     <div className='loginsignup'>
       <div className="loginsignup-container">
-        <h1>Signup</h1>
-        <div className="loginsignup-fields">
-          <input type="text" placeholder='Your Name' />
-          <input type="email" placeholder='Email Address' />
-          <input type="password" placeholder='Password' />
+      <img src={"https://asalapublishers.com/assets/img/logo.png" } alt="Logo" />
+      <h1>Welcome Back!</h1>
+      <p>To keep connected with us </p>
+        <p>please login with your info</p>
+        <div className="loginsignup-logo">
         </div>
-        <button>Register</button>
+        <div className="loginsignup-fields">
+          <label htmlFor="email">Email Address</label>
+          <input type="email" id="email" placeholder='' />
+
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" placeholder='' />
+        </div>
         <p className="loginsignup-login">
-          Already have an account? <Link to="/login">Login here</Link>
+          <span to="/login">Forgot my password?</span>
         </p>
         <div className="loginsignup-agree">
           <input type="checkbox" name='' id='' />
-          <p>By continuing, I agree to the terms of use & privacy policy</p>
+          <p>Keep me signed in</p>
         </div>
+        <button>Sign In</button>
+      </div>
+
+     <div className="create-account-container">
+        <h1>Create Account</h1>
+        <div className="create-account-fields">
+          <div>
+            <label htmlFor="firstName">First Name</label>
+            <input type="text" id="firstName" placeholder='' />
+          </div>
+
+          <div>
+            <label htmlFor="lastName">Last Name</label>
+            <input type="text" id="lastName" placeholder='' />
+          </div>
+
+          <div>
+            <label htmlFor="phoneNumber">Phone Number</label>
+            <input type="text" id="phoneNumber" placeholder='+961 &#8595;' />
+          </div>
+
+          <div>
+            <label htmlFor="city">City</label>
+            <input type="text" id="city" placeholder='' />
+          </div>
+
+          <div>
+            <label htmlFor="region">Region</label>
+            <input type="text" id="region" placeholder='' />
+          </div>
+
+          <div>
+            <label htmlFor="building">Building</label>
+            <input type="text" id="building" placeholder='' />
+          </div>
+            
+          <div>
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" placeholder='' />
+          </div>
+
+          <div>
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <input type="password" id="confirmPassword" placeholder='' />
+          </div>
+        </div>
+        <button>Create Account</button>
+      </div>
+      <div className="footer">
       </div>
     </div>
   );
