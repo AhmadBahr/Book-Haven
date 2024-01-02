57f8d1cc19aa077c2949f4fbda6ee917acede580
Ahmad Bahr <150359856+AhmadBahr@users.noreply.github.com>
2024-01-03 00:16:32 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import Footer from './Components/Footer/Footer';
 import banner_mens from './Components/Assets/banner_mens.png'
 import women_banner from './Components/Assets/banner_women.png'
 import kids_banner from './Components/Assets/banner_kids.png'
+import Profile from './Components/Profile/Profile'
+import ShopOurBooks from'./Components/ShopOurBooks/ShopOurBooks'
 
 
 
@@ -18,7 +20,7 @@ function App() {
   return (
     <div>
       <BrowserRouter>
-        <Navbar />
+       <Navbar/>
         <Routes>
           <Route path='/' element={<Shop />} />
           <Route path='/men' element={<ShopCategory banner={banner_mens} category="men" />} />
@@ -29,7 +31,9 @@ function App() {
           </Route>
           <Route path='/cart' element={<Cart />} />
           <Route path='/login' element={<LoginSignup />} />
-          <Route path="/signup" component={LoginSignup} />
+          <Route path="/signup" component={<LoginSignup/>} />
+          <Route path='/Profile' element={<Profile />} />
+          <Route path='/ShopOurBooks' element={<ShopOurBooks/>}/>
         </Routes>
         <Footer/>
       </BrowserRouter>
