63ed4c9a4b720e5dd0a6835a94d950096c2df468
Ahmad Bahr <150359856+AhmadBahr@users.noreply.github.com>
2024-01-03 17:05:56 +0200

    Update ShopOurBooks.jsx

--- src/Components/ShopOurBooks/ShopOurBooks.jsx
+++ src/Components/ShopOurBooks/ShopOurBooks.jsx
@@ -1,34 +1,88 @@
 import React, { useState } from 'react';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
-import filter from '../Assets/filter_icon.jpg';
+import filter from '../Assets/filter.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faThLarge } from '@fortawesome/free-solid-svg-icons';
-import menu_icon from '../Assets/list.png'
+import menu_icon from '../Assets/menu_icon.svg'
 import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
+import Helium_icon from '../Assets/Helium_icon.jpg'
+import tiffany_icon from '../Assets/tiffany_icon.jpg'
+import felix_icon from '../Assets/felix_icon.jpg'
+import lobizona_icon from '../Assets/lobi_zona.jpg'
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
+
+const ProductCard = ({ name, description, imageUrl, price }) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const [isAddedToCart, setIsAddedToCart] = useState(false);
+	const handleAddToCart = () => {
+
+    setIsAddedToCart(true);
+
+  <div style={{ width: '250px', height: '400px', marginBottom: '20px', borderRadius: '10px', overflow: 'hidden', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', position: 'relative',
+	boxShadow: isHovered ? '0 4px 8px rgba(0, 0, 0, 0.1)' : 'none', }}>
+		 onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    <div>
+      <img src={imageUrl} alt={name} style={{ width: '100%', height: '250px', objectFit: 'cover', borderRadius: '10px' }} />
+      <h4 style={{ textAlign: 'center', margin: '10px 0' }}>Helium</h4>
+      <p style={{ textAlign: 'center', margin: '0 10px' }}>Candy Carson</p>
+      <p style={{ color: '#CC2448', fontSize: '18px', textAlign: 'center', marginTop: '10px' }}>$6.00</p>
+			
+			{isHovered && !isAddedToCart && (
+          <button
+            onClick={handleAddToCart}
+            style={{
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+              background: '#E01F20',
+              color: 'white',
+              border: 'none',
+              borderRadius: '5px',
+              padding: '10px',
+              cursor: 'pointer',
+              display: 'flex',
+              alignItems: 'center',
+            }}
+          >
+            <ShoppingCartIcon style={{ marginRight: '5px' }} /> Add to Cart
+          </button>
+        )}
+        {isAddedToCart && (
+          <p style={{ position: 'absolute', bottom: '10px', left: '50%', transform: 'translateX(-50%)' }}>
+            Added to Cart!
+          </p>
+        )}
+      </div>
+    </div>
+};
+};
 
 
 const ShopOurBooks = () => {
-    const [showCategories, setShowCategories] = useState(false);
-    const [selectedCategories, setSelectedCategories] = useState([]);
+  const [showCategories, setShowCategories] = useState(false);
+  const [selectedCategories, setSelectedCategories] = useState([]);
+
+  const handleCategoriesClick = () => {
+    setShowCategories(!showCategories);
+  };
+ 
   
-    const handleCategoriesClick = () => {
-      setShowCategories(!showCategories);
-    };
-  
-    const handleCategoryCheckboxChange = (category) => {
-      const updatedCategories = [...selectedCategories];
-  
-      if (updatedCategories.includes(category)) {
-        // If category is already selected, remove it
-        updatedCategories.splice(updatedCategories.indexOf(category), 1);
-      } else {
-        // If category is not selected, add it
-        updatedCategories.push(category);
-      }
-  
-      setSelectedCategories(updatedCategories);
-    };
-  
+  const handleCategoryCheckboxChange = (category) => {
+    const updatedCategories = [...selectedCategories];
+
+    if (updatedCategories.includes(category)) {
+
+      updatedCategories.splice(updatedCategories.indexOf(category), 1);
+    } else {
+
+      updatedCategories.push(category);
+    }
+
+    setSelectedCategories(updatedCategories);
+  };
 
   const containerStyle = {
     backgroundImage: 'url("https://asalapublishers.com/uploads/static_seo_pages/1600x360/asala_about4.jpg")',
@@ -76,20 +130,54 @@ const ShopOurBooks = () => {
   const dropdownStyle = {
     position: 'absolute',
     top: '30px',
-    left: '350',
+    left: '485px',
     background: 'white',
     padding: '10px',
     borderRadius: '5px',
     boxShadow: '0 2px 5px rgba(0, 0, 0, 0.1)',
-    width:'150px,'
+    display: 'flex',
+    flexDirection: 'column',
   };
+
+	const productContainerStyle = {
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+		justifyContent: 'center',
+		gap: '5px', 
+	};
+	
+	const upperProductContainerStyle = {
+		display: 'grid',
+		gridTemplateColumns: 'repeat(4, 1fr)',
+		gap: '10px', 
+		marginBottom: '20px',
+		marginLeft: '35px',
+		marginTop:'30px',
+	};
+	
+	const lowerProductContainerStyle = {
+		display: 'grid',
+		gridTemplateColumns: 'repeat(4, 1fr)',
+		gap: '10px', 
+		marginBottom: '20px',
+		marginLeft: '35px',
+		marginTop:'30px',
+	};
+	const productImages = [
+    Helium_icon,
+  	tiffany_icon,
+  	felix_icon,
+  	lobizona_icon,
+];
+
 
 
   return (
     <div>
       <div style={containerStyle} className="contact-us-container">
         <h2 style={titleStyle} className="title">
-          Contact Us
+          Shop Our Books
         </h2>
         <div style={{ marginLeft: '730px', fontSize: '15px' }}>Home . Shop our Books</div>
       </div>
@@ -108,11 +196,8 @@ const ShopOurBooks = () => {
         <img src={menu_icon} alt="Menu" style={{ marginLeft: '10px', marginRight: '10px' }} />
         <div style={{ textAlign: 'center', margin: '0 auto' }}>
 
-
-
-
     <span style={{ color: 'black', marginRight: '10px' }}>Category</span>
-    <FontAwesomeIcon icon={faAngleDown} style={{ cursor: 'pointer', marginRight: '10px' }} />
+    <FontAwesomeIcon icon={faAngleDown} style={{ marginRight: '10px' }} />
 
     <span style={{ color: 'black', marginRight: '10px' }}>Author</span>
     <FontAwesomeIcon icon={faAngleDown}  style={{ marginRight: '10px' }} />
@@ -138,95 +223,125 @@ const ShopOurBooks = () => {
     <span>|</span>
 
     <span style={{ marginLeft: '10px', color: 'black', opacity:'0.5'}}>Show 8 &nbsp;</span>
-  
+
+
     {showCategories && (
-            <div style={{ ...dropdownStyle, display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)', gap: '10px' }}>
-            <p>Choose your favorite category:</p>
-            <div>
-              <label>
-                <input
-                  type="checkbox"
-                  checked={selectedCategories.includes('Category 1')}
-                  onChange={() => handleCategoryCheckboxChange('Category 1')}
-                />{' '}
-                Category 1
-              </label>
-            </div>
-            <div>
-              <label>
-                <input
-                  type="checkbox"
-                  checked={selectedCategories.includes('Category 2')}
-                  onChange={() => handleCategoryCheckboxChange('Category 2')}
-                />{' '}
-                Category 2
-              </label>
-            </div>
-            <div>
-              <label>
-                <input
-                  type="checkbox"
-                  checked={selectedCategories.includes('Category 3')}
-                  onChange={() => handleCategoryCheckboxChange('Category 3')}
-                />{' '}
-                Category 3
-              </label>
-            </div>
-            <div>
-              <label>
-                <input
-                  type="checkbox"
-                  checked={selectedCategories.includes('Category 4')}
-                  onChange={() => handleCategoryCheckboxChange('Category 4')}
-                />{' '}
-                Category 4
-              </label>
-            </div>
-            <div>
-              <label>
-                <input
-                  type="checkbox"
-                  checked={selectedCategories.includes('Category 5')}
-                  onChange={() => handleCategoryCheckboxChange('Category 5')}
-                />{' '}
-                Category 5
-              </label>
-            </div>
-            <div>
-              <label>
-                <input
-                  type="checkbox"
-                  checked={selectedCategories.includes('Category 6')}
-                  onChange={() => handleCategoryCheckboxChange('Category 6')}
-                />{' '}
-                Category 6
-              </label>
-            </div>
-            <div>
-              <label>
-                <input
-                  type="checkbox"
-                  checked={selectedCategories.includes('Category 7')}
-                  onChange={() => handleCategoryCheckboxChange('Category 7')}
-                />{' '}
-                Category 7
-              </label>
-            </div>
-            <div>
-              <label>
-                <input
-                  type="checkbox"
-                  checked={selectedCategories.includes('Category 8')}
-                  onChange={() => handleCategoryCheckboxChange('Category 8')}
-                />{' '}
-                Category 8
-              </label>
-            </div>
-          </div>
-        )}
+   <div style={{ ...dropdownStyle, textAlign: 'left', padding: '0 10px' }}>
+   <p style={{ fontWeight: 'bold', opacity: 0.3, fontSize: '13px', marginBottom: '5px' }}>Choose your favorite category:</p>
+   <div style={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)', gap: '10px', textAlign: 'center' }}>
+     <div>
+        <label>
+          <input
+            type="checkbox"
+            checked={selectedCategories.includes('Category 1')}
+            onChange={() => handleCategoryCheckboxChange('Category 1')}
+          />{' '}
+          Category 1
+        </label>
+      </div>
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            checked={selectedCategories.includes('Category 2')}
+            onChange={() => handleCategoryCheckboxChange('Category 2')}
+          />{' '}
+          Category 2
+        </label>
+      </div>
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            checked={selectedCategories.includes('Category 3')}
+            onChange={() => handleCategoryCheckboxChange('Category 3')}
+          />{' '}
+          Category 3
+        </label>
+      </div>
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            checked={selectedCategories.includes('Category 4')}
+            onChange={() => handleCategoryCheckboxChange('Category 4')}
+          />{' '}
+          Category 4
+        </label>
+      </div>
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            checked={selectedCategories.includes('Category 5')}
+            onChange={() => handleCategoryCheckboxChange('Category 5')}
+          />{' '}
+          Category 5
+        </label>
+      </div>
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            checked={selectedCategories.includes('Category 6')}
+            onChange={() => handleCategoryCheckboxChange('Category 6')}
+          />{' '}
+          Category 6
+        </label>
+      </div>
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            checked={selectedCategories.includes('Category 7')}
+            onChange={() => handleCategoryCheckboxChange('Category 7')}
+          />{' '}
+          Category 7
+        </label>
+      </div>
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            checked={selectedCategories.includes('Category 8')}
+            onChange={() => handleCategoryCheckboxChange('Category 8')}
+          />{' '}
+          Category 8
+        </label>
       </div>
     </div>
+  </div>
+)}
+     </div>
     </div>
+<hr/>
+
+<div style={{ ...productContainerStyle, ...upperProductContainerStyle }}>
+        {[1, 2, 3, 4].map((product, index) => (
+          <ProductCard
+            key={product}
+            name={`Product ${product}`}
+            description={`Description for Product ${product}`}
+            price={9.99 * product}
+            imageUrl={productImages[index]}
+          />
+        ))}
+      </div>
+
+      <div style={{ ...productContainerStyle, ...lowerProductContainerStyle }}>
+        {[5, 6, 7, 8].map((product, index) => (
+          <ProductCard
+            key={product}
+            name={`Product ${product}`}
+            description={`Description for Product ${product}`}
+            price={9.99 * product}
+            imageUrl={productImages[index]}
+          />
+        ))}
+      </div>
+
+      </div>
+
   );
 };
 
