c3c961e51484b70978b0095a4752a1a36d62616c
Ahmad Bahr <150359856+AhmadBahr@users.noreply.github.com>
2024-01-04 17:09:38 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,42 @@
-import './App.css';
-import { Navbar } from './Components/Navbar/Navbar';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Cart from './Pages/Cart';
 import LoginSignup from './Pages/LoginSignup';
 import Product from './Pages/Product';
 import Shop from './Pages/Shop';
 import ShopCategory from './Pages/ShopCategory';
 import Footer from './Components/Footer/Footer';
-import banner_mens from './Components/Assets/banner_mens.png'
-import women_banner from './Components/Assets/banner_women.png'
-import kids_banner from './Components/Assets/banner_kids.png'
-import Profile from './Components/Profile/Profile'
-import ShopOurBooks from'./Components/ShopOurBooks/ShopOurBooks'
-
-
-
+import banner_mens from './Components/Assets/banner_mens.png';
+import women_banner from './Components/Assets/banner_women.png';
+import kids_banner from './Components/Assets/banner_kids.png';
+import Profile from './Components/Profile/Profile';
+import ContactUs from './Components/Contact/Contact';
+import ShopOurBooks from './Components/ShopOurBooks/ShopOurBooks';
+import DetailsPage from './Components/DetailsPage/DetailsPage';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-       <Navbar/>
+      <Router>
+        <Navbar />
         <Routes>
           <Route path='/' element={<Shop />} />
-          <Route path='/men' element={<ShopCategory banner={banner_mens} category="men" />} />
-          <Route path='/women' element={<ShopCategory banner={women_banner}category="women" />} />
-          <Route path='/kids' element={<ShopCategory  banner={kids_banner}category="kid" />} />
-          <Route path="product" element={<Product />}>
-            <Route path=':productId' element={<Product />} />
-          </Route>
+          <Route path='/men' element={<ShopCategory banner={banner_mens} category="About Us" />} />
+          <Route path='/women' element={<ShopCategory banner={women_banner} category="women" />} />
+          <Route path='/kids' element={<ShopCategory banner={kids_banner} category="kid" />} />
           <Route path='/cart' element={<Cart />} />
           <Route path='/login' element={<LoginSignup />} />
-          <Route path="/signup" component={<LoginSignup/>} />
           <Route path='/Profile' element={<Profile />} />
-          <Route path='/ShopOurBooks' element={<ShopOurBooks/>}/>
+          <Route path='/Contact-us' element={<ContactUs />} />
+          <Route path='/ShopOurBooks' element={<ShopOurBooks />} />
+          <Route path="/DetailsPage/*" element={<Product />}>
+          <Route path=":productId" element={<DetailsPage />} />
+          </Route>
+
         </Routes>
-        <Footer/>
-      </BrowserRouter>
+        <Footer />
+      </Router>
     </div>
   );
 }
