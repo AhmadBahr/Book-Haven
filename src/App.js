6a9fa2f4f02b60dea1067deee4f81ef4b3748bba
Ahmad Bahr <150359856+AhmadBahr@users.noreply.github.com>
2024-01-05 10:18:53 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Cart from './Pages/Cart';
 import LoginSignup from './Pages/LoginSignup';
 import Product from './Pages/Product';
-import Shop from './Pages/Shop';
-import ShopCategory from './Pages/ShopCategory';
 import Footer from './Components/Footer/Footer';
-import banner_mens from './Components/Assets/banner_mens.png';
-import women_banner from './Components/Assets/banner_women.png';
-import kids_banner from './Components/Assets/banner_kids.png';
 import Profile from './Components/Profile/Profile';
 import ContactUs from './Components/Contact/Contact';
 import ShopOurBooks from './Components/ShopOurBooks/ShopOurBooks';
@@ -21,10 +16,15 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Shop />} />
-          <Route path='/men' element={<ShopCategory banner={banner_mens} category="About Us" />} />
-          <Route path='/women' element={<ShopCategory banner={women_banner} category="women" />} />
-          <Route path='/kids' element={<ShopCategory banner={kids_banner} category="kid" />} />
+
+          {/* <Route path='/' element={<Shop />} /> */}
+
+          {/* <Route path='/men' element={<ShopCategory banner={banner_mens} category="About Us" />} /> */}
+
+          {/* <Route path='/women' element={<ShopCategory banner={women_banner} category="women" />} /> */}
+
+          {/* <Route path='/kids' element={<ShopCategory banner={kids_banner} category="kid" />} /> */}
+
           <Route path='/cart' element={<Cart />} />
           <Route path='/login' element={<LoginSignup />} />
           <Route path='/Profile' element={<Profile />} />
@@ -33,7 +33,6 @@ function App() {
           <Route path="/DetailsPage/*" element={<Product />}>
           <Route path=":productId" element={<DetailsPage />} />
           </Route>
-
         </Routes>
         <Footer />
       </Router>
