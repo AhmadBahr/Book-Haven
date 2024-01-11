89f6c081a7910a49b1757f403fd4ca9212b2193a
Ahmad Bahr <150359856+AhmadBahr@users.noreply.github.com>
2024-01-11 10:23:56 +0200

    Update Breadcrums.jsx

--- src/Components/Breadcrums/Breadcrums.jsx
+++ src/Components/Breadcrums/Breadcrums.jsx
@@ -1,17 +1,11 @@
-import React from 'react'
-import './Breadcrums.css'
-import ArrowBackOutlinedIcon from '@mui/icons-material/ArrowBackOutlined';
-
-const Breadcrumbs = (props) => {
-      const {product} = props;
-      return (
-        <div className='breadcrumbs-container'>
-        <div style={{ position: 'relative', backgroundColor: '#4A1474', height: '60px' }}>
-          <ArrowBackOutlinedIcon style={{ position: 'absolute', top: '50%', left: '2%', transform: 'translateY(-50%)', fontSize: '25px', color: 'white', marginRight: '5px' }} />
-          <span style={{ position: 'absolute', top: '50%', left: '4%', transform: 'translateY(-50%)', fontSize: '20px', color: 'white' }}>Back</span>
-        </div>
-          </div>
-      );
-    };
-    
-    export default Breadcrumbs;
+.breadcrum {
+    align-items: center;
+    display: flex;
+    gap: 10px;
+    color: #5e5e5e;
+    font-size: 16px;
+    font-weight: 600;
+    margin: 60px 0; 
+    text-transform: capitalize;
+    justify-content: flex-start; 
+}
