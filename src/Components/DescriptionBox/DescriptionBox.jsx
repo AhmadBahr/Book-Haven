4742814781d2b3c56491cdb5dbd0068dde5002e7
Ahmad Bahr <150359856+AhmadBahr@users.noreply.github.com>
2024-01-11 10:21:42 +0200

    Update DescriptionBox.jsx

--- src/Components/DescriptionBox/DescriptionBox.jsx
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -1,21 +1,31 @@
-import React from 'react'
-import './DescriptionBox.css'
+import React from 'react';
+import './DescriptionBox.css';
 
 const DescriptionBox = () => {
   return (
     <div className='descriptionbox'>
-        <div className="descriptionbox-navigator">
-            <div className="descriptionbox-nav-box">Description</div>
-                <div className="descriptionbox-nav-box fade">Reviews (122)</div>
-        </div>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box description-nav">Description</div>
+        <div className="additional-info">
+          <p className="information-text">Additional Information</p>
+          </div>
+      <div className="descriptionbox-nav-box fade">Reviews (0)</div>
+      </div>
+      <div className="rounded-container">
         <div className="descriptionbox-description">
-          <p>An e-commerce website is one that allows people to buy and sell physical goods, services, and digital products over the internet rather than at a brick-and-mortar location.</p>
-            <p>
-            Through an e-commerce website, a business can process orders, accept payments, manage shipping and logistics, and provide customer service.
-            </p>
+          <p className="purple-underlined-text">
+            This book is a part of readable series, which helps the child to pick that is suitable for his reading level, by which he can practice and practice and improve
+            his reading abilities. The child will start reading independently and ascending consequently depending on his own reading level and potentials.
+          </p>
+          <p>
+            The series contians nemerous attractive stories that are full of suspense and human values. It is considered one of the most important series among all readable series in the Arab world, since it is Arabic oriented and not translated.
+          </p>
         </div>
-    </div>
-  )
+        </div>
+        </div>
+
+
+  );
 }
 
-export default DescriptionBox
+export default DescriptionBox;
